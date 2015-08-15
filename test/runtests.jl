f6eafb46dee885d4c25d2c87a88255aacfe4757e
Dominique Orban <dominique.orban@gmail.com>
2015-08-15 15:41:19 -0400

    Warning about models used in tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using JuMP
 import AmplNLReader
 using Base.Test
 
+@printf("For tests to pass, the JuMP and AMPL models must have been written identically.\n")
+@printf("Constraints, if any, must have been declared in the same order.\n")
+@printf("In addition, the AMPL model must have been decoded with preprocessing disabled.\n")
 include("jump_vs_ampl.jl")
