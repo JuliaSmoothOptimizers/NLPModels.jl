23dd28fa625de3aa286555fab14e168026561364
Dominique Orban <dominique.orban@gmail.com>
2016-03-24 13:51:48 -0400

    add a few simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,20 @@ using JuMP
 using AmplNLReader
 using Base.Test
 
+include("genrose.jl")
+model = JuMPNLPModel(genrose())
+for f in fieldnames(model.counters)
+  @assert getfield(model.counters, f) == 0
+end
+
+obj(model, model.meta.x0)
+@assert model.counters.neval_obj == 1
+
+reset!(model)
+@assert model.counters.neval_obj == 0
+
+@test_throws(ErrorException, jth_con(model))
+
 @printf("For tests to pass, the JuMP and AMPL models must have been written identically.\n")
 @printf("Constraints, if any, must have been declared in the same order.\n")
 @printf("In addition, the AMPL model must have been decoded with preprocessing disabled.\n")
