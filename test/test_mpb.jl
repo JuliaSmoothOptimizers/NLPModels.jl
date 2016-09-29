f9a143a7c05bab110d7f116383b75dca91693d67
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-29 13:56:59 -0300

    Test NLPtoMPB on MathProgNLPModel and ADNLPModel

--- test/test_mpb.jl
+++ test/test_mpb.jl
@@ -1,11 +1,15 @@
 using Ipopt
 using JuMP
 
-# pass an AmplModel to IPOPT
-nlp = MathProgNLPModel(hs6())
+# pass an ADNLPModel to IPOPT
+nlp = ADNLPModel(x->dot(x,x), ones(2), c=x->[sum(x)-1], lcon=[0.0], ucon=[0.0])
 show(nlp.meta)
 print(nlp.meta)
 model = NLPtoMPB(nlp, IpoptSolver())
 @assert isa(model, Ipopt.IpoptMathProgModel)
 MathProgBase.optimize!(model)
-@assert MathProgBase.getobjval(model) ≈ 0.0
+@assert MathProgBase.getobjval(model) ≈ 0.5
+
+# Testing NLPtoMPB of a MathProgNLPModel
+nlp = MathProgNLPModel(hs6())
+NLPtoMPB(nlp, IpoptSolver())
