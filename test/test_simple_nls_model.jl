a02e29d05accd52749efa99d4e8f4143a66281d4
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-06-19 11:15:52 -0300

    Add Simple NLS model

---
+++ test/test_simple_nls_model.jl
@@ -0,0 +1,11 @@
+function simple_nls_test()
+  @testset "simple_nls_test" begin
+    F(x) = [x[1] - 1; x[2] - x[1]^2]
+    J(x) = [1.0 0.0; -2*x[1] 1.0]
+    nls = SimpleNLSModel(2, 2, F=F)
+
+    @test residual(nls, ones(2)) == zeros(2)
+  end
+end
+
+simple_nls_test()
