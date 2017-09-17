cfd3a2d272d5d47a3c51f78659c0309174f22224
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-09-17 09:15:36 -0300

    Add constraints to autodiff nls

--- src/autodiff_nlsmodel.jl
+++ src/autodiff_nlsmodel.jl
@@ -2,7 +2,8 @@ using ForwardDiff
 
 export ADNLSModel,
        residual!, jac_residual, jprod_residual!, jtprod_residual!,
-       jac_op_residual, hess_residual, hprod_residual!
+       jac_op_residual, hess_residual, hprod_residual!, cons, cons!, jac_coord,
+       jac, jprod, jprod!, jtprod, jtprod!, hess, hess_coord, hprod, hprod!
 
 type ADNLSModel <: AbstractNLSModel
   meta :: NLPModelMeta
@@ -11,22 +12,33 @@ type ADNLSModel <: AbstractNLSModel
 
   # Function
   F :: Function
+  c :: Function
 end
 
 function ADNLSModel(F :: Function, x0 :: Vector, m :: Int;
                     name :: String = "Generic",
                     lvar :: Vector = fill(-Inf, length(x0)),
-                    uvar :: Vector = fill(Inf, length(x0))
+                    uvar :: Vector = fill(Inf, length(x0)),
+                    c :: Function = (args...)->throw(NotImplementedError("cons")),
+                    lcon :: Vector = Float64[],
+                    ucon :: Vector = Float64[],
+                    y0 :: Vector = zeros(max(length(lcon), length(ucon)))
                    )
   nvar = length(x0)
+  ncon = maximum([length(lcon); length(ucon); length(y0)])
+  if !(length(lcon) == length(ucon) == length(y0))
+    error("lcon, ucon and y0 need to be the same length")
+  end
+  nnzj = nvar * ncon
 
-  meta = NLPModelMeta(nvar, x0=x0, lvar=lvar, uvar=uvar)
+  meta = NLPModelMeta(nvar, x0=x0, lvar=lvar, uvar=uvar, ncon=ncon, y0=y0,
+                      lcon=lcon, ucon=ucon, nnzj=nnzj)
   nls_meta = NLSMeta(m, nvar)
 
-  return ADNLSModel(meta, nls_meta, NLSCounters(), F)
+  return ADNLSModel(meta, nls_meta, NLSCounters(), F, c)
 end
 
-ADNLSModel(F :: Function, n :: Int, m :: Int) = ADNLSModel(F, zeros(n), m)
+ADNLSModel(F :: Function, n :: Int, m :: Int; kwargs...) = ADNLSModel(F, zeros(n), m; kwargs...)
 
 function residual!(nls :: ADNLSModel, x :: AbstractVector, Fx :: AbstractVector)
   increment!(nls, :neval_residual)
@@ -60,4 +72,102 @@ function hprod_residual!(nls :: ADNLSModel, x :: AbstractVector, i :: Int, v :: 
   increment!(nls, :neval_hprod_residual)
   Hiv[:] = ForwardDiff.hessian(x->nls.F(x)[i], x) * v
   return Hiv
+end
+
+function cons(nls :: ADNLSModel, x :: Vector)
+  increment!(nls, :neval_cons)
+  return nls.c(x)
+end
+
+function cons!(nls :: ADNLSModel, x :: Vector, c :: Vector)
+  increment!(nls, :neval_cons)
+  c[1:nls.meta.ncon] = nls.c(x)
+  return c
+end
+
+function jac_coord(nls :: ADNLSModel, x :: Vector)
+  increment!(nls, :neval_jac)
+  J = ForwardDiff.jacobian(nls.c, x)
+  return findnz(J)
+end
+
+function jac(nls :: ADNLSModel, x :: Vector)
+  increment!(nls, :neval_jac)
+  return ForwardDiff.jacobian(nls.c, x)
+end
+
+function jprod(nls :: ADNLSModel, x :: Vector, v :: Vector)
+  increment!(nls, :neval_jprod)
+  return ForwardDiff.jacobian(nls.c, x) * v
+end
+
+function jprod!(nls :: ADNLSModel, x :: Vector, v :: Vector, Jv :: Vector)
+  increment!(nls, :neval_jprod)
+  Jv[1:nls.meta.ncon] = ForwardDiff.jacobian(nls.c, x) * v
+  return Jv
+end
+
+function jtprod(nls :: ADNLSModel, x :: Vector, v :: Vector)
+  increment!(nls, :neval_jtprod)
+  return ForwardDiff.jacobian(nls.c, x)' * v
+end
+
+function jtprod!(nls :: ADNLSModel, x :: Vector, v :: Vector, Jtv :: Vector)
+  increment!(nls, :neval_jtprod)
+  Jtv[1:nls.meta.nvar] = ForwardDiff.jacobian(nls.c, x)' * v
+  return Jtv
+end
+
+function hess(nls :: ADNLSModel, x :: Vector; obj_weight = 1.0, y :: Vector = [])
+  increment!(nls, :neval_hess)
+  Fx = residual(nls, x)
+  Jx = jac_residual(nls, x)
+  Hx = obj_weight == 0.0 ? spzeros(nls.meta.nvar, nls.meta.nvar) : Jx' * Jx * obj_weight
+  if obj_weight != 0.0
+    m = length(Fx)
+    for i = 1:m
+      Hx += obj_weight * Fx[i] * hess_residual(nls, x, i)
+    end
+  end
+  for i = 1:min(length(y), nls.meta.ncon)
+    if y[i] != 0.0
+      Hx += ForwardDiff.hessian(x->nls.c(x)[i], x) * y[i]
+    end
+  end
+  return tril(Hx)
+end
+
+function hess_coord(nls :: ADNLSModel, x :: Vector; obj_weight = 1.0, y :: Vector = [])
+  H = hess(nls, x, obj_weight=obj_weight, y=y)
+  return findnz(H)
+end
+
+function hprod(nls :: ADNLSModel, x :: Vector, v :: Vector;
+    obj_weight = 1.0, y :: Vector = [])
+  Hv = zeros(nls.meta.nvar)
+  return hprod!(nls, x, v, Hv, obj_weight=obj_weight, y=y)
+end
+
+function hprod!(nls :: ADNLSModel, x :: Vector, v :: Vector, Hv :: Vector;
+    obj_weight = 1.0, y :: Vector = [])
+  increment!(nls, :neval_hprod)
+  n = nls.meta.nvar
+  if obj_weight != 0.0
+    Fx = residual(nls, x)
+    Jv = jprod_residual(nls, x, v)
+    @views jtprod_residual!(nls, x, Jv, Hv[1:n])
+    m = length(Fx)
+    Hiv = zeros(n)
+    for i = 1:m
+      hprod_residual!(nls, x, i, v, Hiv)
+      @views Hv[1:n] .= Hv[1:n] .+ Fx[i] * Hiv
+    end
+    Hv[1:n] .*= obj_weight
+  end
+  for i = 1:min(length(y), nls.meta.ncon)
+    if y[i] != 0.0
+      Hv[1:n] += ForwardDiff.hessian(x->nls.c(x)[i], x) * v * y[i]
+    end
+  end
+  return Hv
 end
