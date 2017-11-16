1e84c7c2fca99c3c35abc59cf2a27447979fc2ae
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-11-16 13:24:50 -0200

    Implement constraints on SimpleNLSModel

--- src/simple_nls_model.jl
+++ src/simple_nls_model.jl
@@ -4,11 +4,13 @@ export SimpleNLSModel,
        hprod_residual, hprod_residual!
 
 """
-    nls = SimpleNLSModel(n;  F=F, F! =F!, J=J, Jp=Jp, Jp! =Jp!, Jtp=Jtp, Jtp! =Jtp!)
-    nls = SimpleNLSModel(x0; F=F, F! =F!, J=J, Jp=Jp, Jp! =Jp!, Jtp=Jtp, Jtp! =Jtp!)
+    nls = SimpleNLSModel(n;  F=F, F! =F!, JF=JF, JFp=JFp, JFp! =JFp!,
+    JFtp=JFtp, JFtp! =JFtp!)
+    nls = SimpleNLSModel(x0; F=F, F! =F!, JF=JF, JFp=JFp, JFp! =JFp!,
+    JFtp=JFtp, JFtp! =JFtp!)
 
 Creates a Nonlinear Linear Least Squares model to minimize ‖F(x)‖².
-If J = J(x) is passed, the Jacobian is available.
+If JF = JF(x) is passed, the Jacobian is available.
 """
 type SimpleNLSModel <: AbstractNLSModel
   meta :: NLPModelMeta
@@ -17,14 +19,25 @@ type SimpleNLSModel <: AbstractNLSModel
 
   F :: Function
   F! :: Function
+  JF :: Function
+  JFp :: Function
+  JFp! :: Function
+  JFtp :: Function
+  JFtp! :: Function
+  Hi :: Function # Hi(x, i) return the Hessian (tri-inf)
+  Hip :: Function # Hip(x, i, v) returns the product
+  Hip! :: Function # Hip!(x, i, v, Hiv) computes the product
+  c :: Function
+  c! :: Function
   J :: Function
+  Jcoord :: Function
   Jp :: Function
   Jp! :: Function
   Jtp :: Function
   Jtp! :: Function
-  Hi :: Function # Hi(x, i) return the Hessian (tri-inf)
-  Hip :: Function # Hip(x, i, v) returns the product
-  Hip! :: Function # Hip!(x, i, v, Hiv) computes the product
+  Hc :: Function # Hessian for the constraints
+  Hcp :: Function
+  Hcp! :: Function
 end
 
 NotImplemented(args...) = throw(NotImplementedError(""))
@@ -32,21 +45,44 @@ NotImplemented(args...) = throw(NotImplementedError(""))
 function SimpleNLSModel(x0 :: AbstractVector, nequ :: Int;
                         lvar :: Vector = fill(-Inf, length(x0)),
                         uvar :: Vector = fill(Inf, length(x0)),
+                        lcon :: Vector = Float64[],
+                        ucon :: Vector = Float64[],
+                        y0 :: Vector = zeros(max(length(lcon), length(ucon))),
                         F :: Function = NotImplemented,
                         F! :: Function = NotImplemented,
-                        J :: Function = NotImplemented,
-                        Jp :: Function = NotImplemented,
-                        Jp! :: Function = NotImplemented,
-                        Jtp :: Function = NotImplemented,
-                        Jtp! :: Function = NotImplemented,
+                        JF :: Function = NotImplemented,
+                        JFp :: Function = NotImplemented,
+                        JFp! :: Function = NotImplemented,
+                        JFtp :: Function = NotImplemented,
+                        JFtp! :: Function = NotImplemented,
                         Hi :: Function = NotImplemented,
                         Hip :: Function = NotImplemented,
-                        Hip! :: Function = NotImplemented
+                        Hip! :: Function = NotImplemented,
+                        c::Function = NotImplemented,
+                        c!::Function = NotImplemented,
+                        J::Function = NotImplemented,
+                        Jcoord::Function = NotImplemented,
+                        Jp::Function = NotImplemented,
+                        Jp!::Function = NotImplemented,
+                        Jtp::Function = NotImplemented,
+                        Jtp!::Function = NotImplemented,
+                        Hc :: Function = NotImplemented,
+                        Hcp :: Function = NotImplemented,
+                        Hcp! :: Function = NotImplemented
                        )
-  meta = NLPModelMeta(length(x0), x0=x0, lvar=lvar, uvar=uvar)
-  nls_meta = NLSMeta(nequ, length(x0))
+  nvar = length(x0)
+  ncon = maximum([length(lcon); length(ucon); length(y0)])
+  if !(length(lcon) == length(ucon) == length(y0))
+    error("lcon, ucon and y0 need to be the same length")
+  end
+  nnzj = nvar * ncon
 
-  return SimpleNLSModel(meta, nls_meta, NLSCounters(), F, F!, J, Jp, Jp!, Jtp, Jtp!, Hi, Hip, Hip!)
+  meta = NLPModelMeta(nvar, x0=x0, lvar=lvar, uvar=uvar, ncon=ncon,
+                      y0=y0, lcon=lcon, ucon=ucon, nnzj=nnzj)
+  nls_meta = NLSMeta(nequ, nvar)
+  return SimpleNLSModel(meta, nls_meta, NLSCounters(), F, F!, JF, JFp,
+                        JFp!, JFtp, JFtp!, Hi, Hip, Hip!, c, c!, J,
+                        Jcoord, Jp, Jp!, Jtp, Jtp!, Hc, Hcp, Hcp!)
 end
 
 SimpleNLSModel(nvar :: Int, nequ :: Int; kwargs...) = SimpleNLSModel(zeros(nvar), nequ; kwargs...)
@@ -64,29 +100,31 @@ end
 
 function jac_residual(nls :: SimpleNLSModel, x :: AbstractVector)
   increment!(nls, :neval_jac_residual)
-  return nls.J(x)
+  return nls.JF(x)
 end
 
 function jprod_residual(nls :: SimpleNLSModel, x :: AbstractVector, v :: AbstractVector)
   increment!(nls, :neval_jprod_residual)
-  return nls.Jp(x, v)
+  return nls.JFp(x, v)
 end
 
-function jprod_residual!(nls :: SimpleNLSModel, x :: AbstractVector, v :: AbstractVector, Jv :: AbstractVector)
+function jprod_residual!(nls :: SimpleNLSModel, x :: AbstractVector, v
+                         :: AbstractVector, JFv :: AbstractVector)
   increment!(nls, :neval_jprod_residual)
-  nls.Jp!(x, v, Jv)
-  return Jv
+  nls.JFp!(x, v, JFv)
+  return JFv
 end
 
 function jtprod_residual(nls :: SimpleNLSModel, x :: AbstractVector, v :: AbstractVector)
   increment!(nls, :neval_jtprod_residual)
-  return nls.Jtp(x, v)
+  return nls.JFtp(x, v)
 end
 
-function jtprod_residual!(nls :: SimpleNLSModel, x :: AbstractVector, v :: AbstractVector, Jtv :: AbstractVector)
+function jtprod_residual!(nls :: SimpleNLSModel, x :: AbstractVector, v
+                          :: AbstractVector, JFtv :: AbstractVector)
   increment!(nls, :neval_jtprod_residual)
-  nls.Jtp!(x, v, Jtv)
-  return Jtv
+  nls.JFtp!(x, v, JFtv)
+  return JFtv
 end
 
 function hess_residual(nls :: SimpleNLSModel, x :: AbstractVector, i :: Int)
@@ -103,4 +141,108 @@ function hprod_residual!(nls :: SimpleNLSModel, x :: AbstractVector, i :: Int, v
   increment!(nls, :neval_hprod_residual)
   nls.Hip!(x, i, v, Hiv)
   return Hiv
+end
+
+function cons(nls :: SimpleNLSModel, x :: Vector)
+  increment!(nls, :neval_cons)
+  return nls.c(x)
+end
+
+function cons!(nls :: SimpleNLSModel, x :: Vector, c :: Vector)
+  increment!(nls, :neval_cons)
+  return nls.c!(x, c)
+end
+
+function jac_coord(nls :: SimpleNLSModel, x :: Vector)
+  increment!(nls, :neval_jac)
+  return nls.Jcoord(x)
+end
+
+function jac(nls :: SimpleNLSModel, x :: Vector)
+  increment!(nls, :neval_jac)
+  return nls.J(x)
+end
+
+function jprod(nls :: SimpleNLSModel, x :: Vector, v :: Vector)
+  increment!(nls, :neval_jprod)
+  return nls.Jp(x, v)
+end
+
+function jprod!(nls :: SimpleNLSModel, x :: Vector, v :: Vector, Jv :: Vector)
+  increment!(nls, :neval_jprod)
+  return nls.Jp!(x, v, Jv)
+end
+
+function jtprod(nls :: SimpleNLSModel, x :: Vector, v :: Vector)
+  increment!(nls, :neval_jtprod)
+  return nls.Jtp(x, v)
+end
+
+function jtprod!(nls :: SimpleNLSModel, x :: Vector, v :: Vector, Jtv :: Vector)
+  increment!(nls, :neval_jtprod)
+  return nls.Jtp!(x, v, Jtv)
+end
+
+function hess(nls :: SimpleNLSModel, x :: Vector; obj_weight = 1.0, y :: Vector = [])
+  increment!(nls, :neval_hess)
+  Fx = residual(nls, x)
+  Jx = jac_residual(nls, x)
+  Hx = obj_weight == 0.0 ? spzeros(nls.meta.nvar, nls.meta.nvar) : Jx' * Jx * obj_weight
+  if obj_weight != 0.0
+    m = length(Fx)
+    for i = 1:m
+      Hx += obj_weight * Fx[i] * hess_residual(nls, x, i)
+    end
+  end
+  if length(y) > 0
+    Hx += nls.Hc(x, y=y)
+  end
+  return tril(Hx)
+end
+
+function hess_coord(nls :: SimpleNLSModel, x :: Vector; obj_weight = 1.0, y :: Vector = [])
+  H = hess(nls, x, obj_weight=obj_weight, y=y)
+  return findnz(H)
+end
+
+function hprod(nls :: SimpleNLSModel, x :: Vector, v :: Vector;
+    obj_weight = 1.0, y :: Vector = [])
+  increment!(nls, :neval_hprod)
+  n = nls.meta.nvar
+  Hv = length(y) > 0 ? nls.Hcp(x, v, y=y) : zeros(n)
+  if obj_weight != 0.0
+    Fx = residual(nls, x)
+    Jv = jprod_residual(nls, x, v)
+    @views Hv[1:n] .+= obj_weight * jtprod_residual(nls, x, Jv)
+    m = length(Fx)
+    Hiv = zeros(n)
+    for i = 1:m
+      hprod_residual!(nls, x, i, v, Hiv)
+      @views Hv[1:n] .+= (obj_weight * Fx[i]) * Hiv
+    end
+  end
+  return Hv
+end
+
+function hprod!(nls :: SimpleNLSModel, x :: Vector, v :: Vector, Hv :: Vector;
+    obj_weight = 1.0, y :: Vector = [])
+  increment!(nls, :neval_hprod)
+  n = nls.meta.nvar
+  if length(y) > 0
+    nls.Hcp!(x, v, Hv, y=y)
+  else
+    @views fill!(Hv[1:n], 0.0)
+  end
+  if obj_weight != 0.0
+    Fx = residual(nls, x)
+    Jv = jprod_residual(nls, x, v)
+    @views Hv[1:n] .+= obj_weight * jtprod_residual(nls, x, Jv)
+    m = length(Fx)
+    Hiv = zeros(n)
+    for i = 1:m
+      hprod_residual!(nls, x, i, v, Hiv)
+      @views Hv[1:n] .+= (obj_weight * Fx[i]) * Hiv
+    end
+  end
+  return Hv
 end
--- test/test_simple_nls_model.jl
+++ test/test_simple_nls_model.jl
@@ -1,8 +1,8 @@
 function simple_nls_test()
   @testset "simple_nls_test" begin
     F(x) = [x[1] - 1; x[2] - x[1]^2]
-    J(x) = [1.0 0.0; -2*x[1] 1.0]
-    nls = SimpleNLSModel(2, 2, F=F)
+    JF(x) = [1.0 0.0; -2*x[1] 1.0]
+    nls = SimpleNLSModel(2, 2, F=F, JF=JF)
 
     @test residual(nls, ones(2)) == zeros(2)
   end
