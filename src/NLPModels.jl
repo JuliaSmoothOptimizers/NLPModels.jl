3b3deb799e68363e33caf6d064d57a7cd879d349
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-09-25 16:21:03 -0300

    Use @closure on jac_op and hess_op

--- src/NLPModels.jl
+++ src/NLPModels.jl
@@ -3,10 +3,7 @@ __precompile__()
 
 module NLPModels
 
-using LinearAlgebra
-using LinearOperators
-using Printf
-using SparseArrays
+using LinearAlgebra, LinearOperators, Printf, SparseArrays, FastClosures
 
 export AbstractNLPModelMeta, NLPModelMeta, AbstractNLPModel, Counters
 export reset!, sum_counters,
@@ -219,8 +216,8 @@ The resulting object may be used as if it were a matrix, e.g., `J * v` or
 `J' * v`.
 """
 function jac_op(nlp :: AbstractNLPModel, x :: AbstractVector)
-  prod = v -> jprod(nlp, x, v)
-  ctprod = v -> jtprod(nlp, x, v)
+  prod = @closure v -> jprod(nlp, x, v)
+  ctprod = @closure v -> jtprod(nlp, x, v)
   F1 = typeof(prod)
   F3 = typeof(ctprod)
   return LinearOperator{Float64,F1,Nothing,F3}(nlp.meta.ncon, nlp.meta.nvar,
@@ -236,8 +233,8 @@ operations.
 """
 function jac_op!(nlp :: AbstractNLPModel, x :: AbstractVector,
                  Jv :: AbstractVector, Jtv :: AbstractVector)
-  prod = v -> jprod!(nlp, x, v, Jv)
-  ctprod = v -> jtprod!(nlp, x, v, Jtv)
+  prod = @closure v -> jprod!(nlp, x, v, Jv)
+  ctprod = @closure v -> jtprod!(nlp, x, v, Jtv)
   F1 = typeof(prod)
   F3 = typeof(ctprod)
   return LinearOperator{Float64,F1,Nothing,F3}(nlp.meta.ncon, nlp.meta.nvar,
@@ -315,7 +312,7 @@ with σ = obj_weight.
 """
 function hess_op(nlp :: AbstractNLPModel, x :: AbstractVector;
                  obj_weight :: Float64=1.0, y :: AbstractVector=zeros(nlp.meta.ncon))
-  prod = v -> hprod(nlp, x, v; obj_weight=obj_weight, y=y)
+  prod = @closure v -> hprod(nlp, x, v; obj_weight=obj_weight, y=y)
   F = typeof(prod)
   return LinearOperator{Float64,F,Nothing,Nothing}(nlp.meta.nvar, nlp.meta.nvar,
                                                    true, true, prod, nothing, nothing)
@@ -335,7 +332,7 @@ with σ = obj_weight.
 """
 function hess_op!(nlp :: AbstractNLPModel, x :: AbstractVector, Hv :: AbstractVector;
                  obj_weight :: Float64=1.0, y :: AbstractVector=zeros(nlp.meta.ncon))
-  prod = v -> hprod!(nlp, x, v, Hv; obj_weight=obj_weight, y=y)
+  prod = @closure v -> hprod!(nlp, x, v, Hv; obj_weight=obj_weight, y=y)
   F = typeof(prod)
   return LinearOperator{Float64,F,Nothing,Nothing}(nlp.meta.nvar, nlp.meta.nvar,
                                                    true, true, prod, nothing, nothing)
