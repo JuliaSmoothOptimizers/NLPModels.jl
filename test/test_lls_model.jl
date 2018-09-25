4d3271d74e81b17a7479abee37bf90532475b7a8
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-09-25 15:39:16 -0300

    Fix coord functions

--- test/test_lls_model.jl
+++ test/test_lls_model.jl
@@ -1,14 +1,21 @@
 function lls_test()
   @testset "lls_test" begin
-    A = rand(10, 3)
-    b = rand(10)
-    nls = LLSModel(A, b)
-    x = rand(3)
+    for A = [rand(10, 3), sprand(10, 3, 0.5)], C = [rand(1, 3), sprand(1, 3, 1.0)]
+      b = rand(10)
+      nls = LLSModel(A, b, C=C, lcon=zeros(1), ucon=zeros(1))
+      x = rand(3)
 
-    @test isapprox(A * x - b, residual(nls, x), rtol=1e-8)
-    @test isapprox(A, jac_residual(nls, x), rtol=1e-8)
-    for i = 1:10
-      @test isapprox(zeros(3, 3), hess_residual(nls, x, i), rtol=1e-8)
+      @test isapprox(A * x - b, residual(nls, x), rtol=1e-8)
+      @test A == jac_residual(nls, x)
+      for i = 1:10
+        @test isapprox(zeros(3, 3), hess_residual(nls, x, i), rtol=1e-8)
+      end
+
+      I, J, V = jac_coord(nls, x)
+      @test sparse(I, J, V) == C
+
+      I, J, V = hess_coord(nls, x)
+      @test sparse(I, J, V) == tril(A' * A)
     end
   end
 end
