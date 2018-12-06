762f7a121ea6f97850d9efba7d6ac25d8e3cd812
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-12-06 16:59:35 -0200

    Remove redundancies on test_not_implemented

--- test/test_not_implemented.jl
+++ test/test_not_implemented.jl
@@ -9,38 +9,25 @@ function test_not_implemented()
   @test_throws(NotImplementedError, lagscale(model, 1.0))
 
   global model
-  for meth in [:obj, :grad, :hess, :hess_coord, :chess]
-    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0]))
-  end
-
-  for meth in [:grad!, :hprod]
-    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0], [2.0]))
-  end
-
-  for meth in [:hprod!]
-    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0], [2.0], [3.0]))
-  end
-
-  for meth in [:cons,  :jac, :jac_coord, :varscale, :conscale,
-               :residual]
+  for meth in [:cons,  :jac, :jac_coord, :varscale, :conscale, :residual]
     @eval @test_throws(NotImplementedError, $meth(model, [0.0]))
   end
   for meth in [:cons!, :jprod, :jtprod, :residual!, :jprod_residual, :jtprod_residual]
-    @eval @test_throws(NotImplementedError, $meth(model, [0], [1]))
+    @eval @test_throws(NotImplementedError, $meth(model, [0.0], [1.0]))
   end
-  for meth in [:jth_con, :jth_congrad, :jth_sparse_congrad, :hess_residual]
-    @eval @test_throws(NotImplementedError, $meth(model, 0, [1]))
+  for meth in [:obj, :grad, :hess, :hess_coord, :chess, :jth_con, :jth_congrad, :jth_sparse_congrad, :hess_residual]
+    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0]))
   end
-  for meth in [:jth_congrad!, :hprod_residual]
-    @eval @test_throws(NotImplementedError, $meth(model, 0, [1], [2]))
-  end
-  @test_throws(NotImplementedError, hprod_residual!(model, 0, [1], [2], [3]))
   for meth in [:jprod!, :jtprod!, :ghjvprod, :jprod_residual!, :jtprod_residual!]
-    @eval @test_throws(NotImplementedError, $meth(model, [0], [1], [2]))
+    @eval @test_throws(NotImplementedError, $meth(model, [0.0], [1.0], [2.0]))
   end
-  @test_throws(NotImplementedError, jth_hprod(model, 0, [1], [2]))
-  @test_throws(NotImplementedError, jth_hprod!(model, 0, [1], [2], [3]))
-  @test_throws(NotImplementedError, ghjvprod!(model, [0], [1], [2], [3]))
+  for meth in [:grad!, :hprod, :jth_hprod, :jth_congrad!, :hprod_residual]
+    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0], [2.0]))
+  end
+  @test_throws(NotImplementedError, ghjvprod!(model, [0.0], [1.0], [2.0], [3.0]))
+  for meth in [:hprod!, :hprod_residual!, :jth_hprod!]
+    @eval @test_throws(NotImplementedError, $meth(model, 0, [1.0], [2.0], [3.0]))
+  end
 end
 
 test_not_implemented()
