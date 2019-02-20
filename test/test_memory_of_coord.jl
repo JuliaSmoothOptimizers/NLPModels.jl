67df51cecdd6df57296a320392a8c32a64d12d32
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-02-20 16:42:13 -0500

    [skip ci] Simple memory tests for coord-improvements and add to docs

---
+++ test/test_memory_of_coord.jl
@@ -0,0 +1,40 @@
+function test_memory_of_coord_of_nlp(nlp :: AbstractNLPModel)
+  n = nlp.meta.nvar
+  m = nlp.meta.nvar
+
+  x = 10 * [-(-1.0)^i for i = 1:n]
+  y = [-(-1.0)^i for i = 1:m]
+
+  # Hessian unconstrained test
+  rows, cols, vals = hess_coord(nlp, x)
+  al1 = @allocated rows, cols, vals = hess_coord(nlp, x)
+  V = zeros(nlp.meta.nnzh)
+  hess_coord!(nlp, x, rows, cols, V)
+  al2 = @allocated hess_coord!(nlp, x, rows, cols, V)
+  @test al2 < al1
+
+  if m > 0
+    rows, cols, vals = hess_coord(nlp, x, y=y)
+    al1 = @allocated rows, cols, vals = hess_coord(nlp, x, y=y)
+    hess_coord!(nlp, x, rows, cols, V, y=y)
+    al2 = @allocated hess_coord!(nlp, x, rows, cols, V, y=y)
+    @test al2 < al1
+
+    rows, cols, vals = jac_coord(nlp, x)
+    al1 = @allocated rows, cols, vals = jac_coord(nlp, x)
+    V = zeros(nlp.meta.nnzj)
+    jac_coord!(nlp, x, rows, cols, vals)
+    al2 = @allocated jac_coord!(nlp, x, rows, cols, vals)
+    @test al2 < al1
+  end
+end
+
+function test_memory_of_coord()
+  @testset "Memory of coordinate inplace functions" begin
+    for p in [:HS5, :HS6, :HS10, :HS11, :HS14]
+      @info("Testing $p")
+      nlp = eval(p)()
+      test_memory_of_coord_of_nlp(nlp)
+    end
+  end
+end
