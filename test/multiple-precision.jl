2f07192913cb698e141bd8b482627c86b67f53f1
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-02-25 14:43:06 -0300

    Test and fix multiple precision in matrix_op

--- test/multiple-precision.jl
+++ test/multiple-precision.jl
@@ -5,11 +5,15 @@ function multiple_precision(nlp :: AbstractNLPModel;
     @test typeof(obj(nlp, x)) == T
     @test eltype(grad(nlp, x)) == T
     @test eltype(hess(nlp, x)) == T
+    @test eltype(hess_op(nlp, x)) == T
     if nlp.meta.ncon > 0
+      y = ones(T, nlp.meta.ncon)
       @test eltype(cons(nlp, x)) == T
       @test eltype(jac(nlp, x)) == T
-      @test eltype(hess(nlp, x, ones(T, nlp.meta.ncon))) == T
-      @test eltype(hess(nlp, x, ones(T, nlp.meta.ncon), obj_weight=one(T))) == T
+      @test eltype(jac_op(nlp, x)) == T
+      @test eltype(hess(nlp, x, y)) == T
+      @test eltype(hess(nlp, x, y, obj_weight=one(T))) == T
+      @test eltype(hess_op(nlp, x, y)) == T
     end
   end
 end
@@ -20,12 +24,17 @@ function multiple_precision(nls :: AbstractNLSModel;
     x = ones(T, nls.meta.nvar)
     @test eltype(residual(nls, x)) == T
     @test eltype(jac_residual(nls, x)) == T
+    @test eltype(jac_op_residual(nls, x)) == T
     @test eltype(hess_residual(nls, x, ones(T, nls.nls_meta.nequ))) == T
+    for i = 1:nls.nls_meta.nequ
+      @test eltype(hess_op_residual(nls, x, i)) == T
+    end
     @test typeof(obj(nls, x)) == T
     @test eltype(grad(nls, x)) == T
     if nls.meta.ncon > 0
       @test eltype(cons(nls, x)) == T
       @test eltype(jac(nls, x)) == T
+      @test eltype(jac_op(nls, x)) == T
     end
   end
 end
