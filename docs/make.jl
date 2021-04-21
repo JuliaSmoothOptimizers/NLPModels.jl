c16b5f2dd25083163ec24c9f904ee6e9bb249678
Dominique <dominique.orban@gmail.com>
2021-04-21 15:09:22 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(
   modules = [NLPModels],
   doctest = true,
   linkcheck = false,
-  strict = true,
+  strict = false,
   format = Documenter.HTML(
     assets = ["assets/style.css"],
     prettyurls = get(ENV, "CI", nothing) == "true",
