3e03bfee914962ff498dd6165f90ba28c904813c
tmigot <tangi.migot@gmail.com>
2021-07-20 05:13:07 +0200

    add getter for nls.nls_meta and tests

--- src/nls/tools.jl
+++ src/nls/tools.jl
@@ -1,1 +1,13 @@
-# Empty
+for field in fieldnames(NLSMeta)
+  meth = Symbol("get_", field)
+  @eval begin
+    @doc """
+        $($meth)(nls)
+        $($meth)(nls_meta)
+    Return the value $($(QuoteNode(field))) from nls\\_meta or nls.nls\\_meta.
+    """
+    $meth(nls_meta::NLSMeta) = getproperty(nls_meta, $(QuoteNode(field)))
+  end
+  @eval $meth(nls::AbstractNLSModel) = $meth(nls.nls_meta)
+  @eval export $meth
+end
---
+++ test/nls/tools.jl
@@ -0,0 +1,19 @@
+mutable struct DummyNLSModel{T, S} <: AbstractNLSModel{T, S}
+  nls_meta::NLSMeta{T, S}
+end
+
+@testset "Problem type functions" begin
+  foo_list = [:nequ, :nvar, :x0, :nnzj, :nnzh, :nln, :nnln, :lin, :nlin]
+  meta_list = [
+    NLSMeta(2, 2),
+    NLSMeta(2, 2, x0 = zeros(2)),
+    NLSMeta(2, 3, lin = [1; 3]),
+    NLSMeta(2, 3, nln = [1; 3]),
+    NLSMeta(1, 4, nnzh = 3, nnzj = 2),
+  ]
+  for f in fieldnames(NLSMeta), (j, meta) in enumerate(meta_list)
+    nls = DummyNLSModel(meta)
+    @test eval(Meta.parse("get_" * string(f)))(meta) == getproperty(meta, f)
+    @test eval(Meta.parse("get_" * string(f)))(nls) == getproperty(meta, f)
+  end
+end
