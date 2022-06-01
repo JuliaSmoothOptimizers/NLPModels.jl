effbd4e76c1698a2f3956a5444eaba6a1461552f
AntoninKns <antonin.kenens@etu.enseeiht.fr>
2022-06-01 11:24:17 -0400

    Adding more code cover

--- test/nlp/counters.jl
+++ test/nlp/counters.jl
@@ -8,6 +8,14 @@
   obj(nlp, nlp.meta.x0)
   grad(nlp, nlp.meta.x0)
   @test sum_counters(nlp) == 2
+
+  for counter in fieldnames(Counters)
+    increment!(nlp, counter)
+  end
+  # sum all counters of problem `nlp` except 
+  # `cons`, `jac`, `jprod` and `jtprod` = 20-4+2
+  @test sum_counters(nlp) == 18
+
   reset!(nlp)
   @test sum_counters(nlp) == 0
 end
