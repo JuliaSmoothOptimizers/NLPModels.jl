a4db1b08d543d1b0c1838d647ab5dc8f6d59fb5a
AntoninKns <antonin.kenens@etu.enseeiht.fr>
2022-06-01 14:04:28 -0400

    Removing useless functions and adding inline

--- test/nls/counters.jl
+++ test/nls/counters.jl
@@ -27,16 +27,3 @@
   reset!(nls)
   @test sum_counters(nls) == 0
 end
-
-@testset "Basic increment of NLSCounters" begin
-  nls = SimpleNLSModel()
-  increment_neval_obj!(nls)
-  increment_neval_residual!(nls)
-  increment_neval_jac_residual!(nls)
-  @test neval_obj(nls) == 1
-  @test neval_residual(nls) == 1
-  @test neval_jac_residual(nls) == 1
-  @test sum_counters(nls) == 3
-  reset!(nls)
-  @test sum_counters(nls) == 0
-end
