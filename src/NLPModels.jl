3ff24c1c5e07b275252376edcdaa023f9bc2ef96
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-19 02:29:52 +0530

    Include files from both nlp and nls directories

--- src/NLPModels.jl
+++ src/NLPModels.jl
@@ -44,11 +44,8 @@ Base type for a nonlinear least-squares model.
 """
 abstract type AbstractNLSModel{T, S} <: AbstractNLPModel{T, S} end
 
-for f in ["utils", "api", "counters", "meta", "show", "tools", "dense"]
-  include("nlp/$f.jl")
-end
-
 for f in ["utils", "api", "counters", "meta", "show", "tools"]
+  include("nlp/$f.jl")
   include("nls/$f.jl")
 end
 
