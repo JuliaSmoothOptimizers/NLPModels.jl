6ed990a55afa9bb1dca98da38d483a3f5fc46df4
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-12-05 10:04:43 -0600

    Revert "Update `jac_lin_coord` methods (#501)"
    This reverts commit 3091fb0a9a40135d55753a369bd59cdcbe8e5bda.

--- test/nlp/simple-model.jl
+++ test/nlp/simple-model.jl
@@ -139,8 +139,8 @@ function NLPModels.jac_nln_coord!(nlp::SimpleNLPModel, x::AbstractVector, vals::
   return vals
 end
 
-function NLPModels.jac_lin_coord!(nlp::SimpleNLPModel, vals::AbstractVector)
-  @lencheck 2 vals
+function NLPModels.jac_lin_coord!(nlp::SimpleNLPModel, x::AbstractVector, vals::AbstractVector)
+  @lencheck 2 x vals
   increment!(nlp, :neval_jac_lin)
   vals .= [1, -2]
   return vals
@@ -159,11 +159,16 @@ function NLPModels.jprod_nln!(
   return Jv
 end
 
-function NLPModels.jprod_lin!(nlp::SimpleNLPModel, v::AbstractVector, Jv::AbstractVector)
-  @lencheck 2 v
+function NLPModels.jprod_lin!(
+  nlp::SimpleNLPModel,
+  x::AbstractVector,
+  v::AbstractVector,
+  Jv::AbstractVector,
+)
+  @lencheck 2 x v
   @lencheck 1 Jv
   increment!(nlp, :neval_jprod_lin)
-  Jv[1] = v[1] - 2 * v[2]
+  Jv .= [v[1] - 2 * v[2]]
   return Jv
 end
 
@@ -180,8 +185,13 @@ function NLPModels.jtprod_nln!(
   return Jtv
 end
 
-function NLPModels.jtprod_lin!(nlp::SimpleNLPModel, v::AbstractVector, Jtv::AbstractVector)
-  @lencheck 2 Jtv
+function NLPModels.jtprod_lin!(
+  nlp::SimpleNLPModel,
+  x::AbstractVector,
+  v::AbstractVector,
+  Jtv::AbstractVector,
+)
+  @lencheck 2 x Jtv
   @lencheck 1 v
   increment!(nlp, :neval_jtprod_lin)
   Jtv .= [v[1]; -2 * v[1]]
